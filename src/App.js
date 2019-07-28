1b15891f9bd0cf9cbb56fc513384345516b81b4a
BLACKBOX\dakil <dakilangmanok@gmail.com>
2019-07-28 16:41:44 -0400

    Added a switch handler.

--- src/App.js
+++ src/App.js
@@ -7,15 +7,20 @@ class App extends Component {
     people: [
       { name: "Patrick", age: 38 },
       { name: "John", age: 25 },
-      { name: "Jane", age: 29 }
+      { name: 'Jane', age: 29 }
     ]
-  };
+  }
+
+  switchNameHandler = () => {
+    console.log('was clicked!');
+  }
+
   render() {
     return (
       <div className="App">
         <h1> Hello world from React! </h1>
         <p> I'm really working!</p>
-        <button>Switch Name</button>
+        <button onClick={}>Switch Name</button>
         {/* You can pass anything not just text between the opening and closing tag. */}
         <Person name={this.state.people[0].name} age={this.state.people[0].age}>
           My Hobbies: piano
