a08150e01157bf64ed986299be708930de5bc0b1
BLACKBOX\dakil <dakilangmanok@gmail.com>
2019-07-28 13:53:45 -0400

    Added a wrapper, and a child paragraph.

--- src/Person/Person.js
+++ src/Person/Person.js
@@ -3,9 +3,12 @@ import React from 'react';
 const person = props => {
   // The + 8 is so 0 is never a number. Someone who is 0 can't talk.
   return (
-    <p>
-      I'm {props.name} and I am {props.age} years old!
-    </p>
+    <div>
+      <p>
+        I'm {props.name} and I am {props.age} years old!
+      </p>
+      <p>{props.children}</p>
+    </div>
   );
 };
 
