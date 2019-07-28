b14257a8d09791e21a8be3332e6bc32ce533fd83
BLACKBOX\dakil <dakilangmanok@gmail.com>
2019-07-28 16:19:00 -0400

    Integrated prettier with es lint. Will change the quotes later.

--- src/App.js
+++ src/App.js
@@ -1,15 +1,15 @@
-import React, { Component } from 'react';
-import './App.css';
-import Person from './Person/Person';
+import React, { Component } from "react";
+import "./App.css";
+import Person from "./Person/Person";
 
 class App extends Component {
   state = {
     people: [
-      { name: 'Patrick', age: 38 },
-      { name: 'John', age: 25 },
-      { name: 'Jane', age: 29 }],
+      { name: "Patrick", age: 38 },
+      { name: "John", age: 25 },
+      { name: "Jane", age: 29 }
+    ]
   };
-
   render() {
     return (
       <div className="App">
@@ -20,8 +20,14 @@ class App extends Component {
         <Person name={this.state.people[0].name} age={this.state.people[0].age}>
           My Hobbies: piano
         </Person>
-        <Person name={this.state.people[1].name} age={this.state.people[1].age} />
-        <Person name={this.state.people[2].name} age={this.state.people[2].age} />
+        <Person
+          name={this.state.people[1].name}
+          age={this.state.people[1].age}
+        />
+        <Person
+          name={this.state.people[2].name}
+          age={this.state.people[2].age}
+        />
       </div>
     );
   }
--- src/Person/Person.js
+++ src/Person/Person.js
@@ -1,4 +1,4 @@
-import React from 'react';
+import React from "react";
 
 const person = props => {
   // The + 8 is so 0 is never a number. Someone who is 0 can't talk.
