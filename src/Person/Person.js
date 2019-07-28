10fbe1c3f33dd46fc30aa9812827b04e46f1699f
BLACKBOX\dakil <dakilangmanok@gmail.com>
2019-07-28 13:39:49 -0400

    Added a props, and dynamic name and age.

--- src/Person/Person.js
+++ src/Person/Person.js
@@ -1,8 +1,12 @@
 import React from 'react';
 
-const person = () => {
-	// The + 8 is so 0 is never a number. Someone who is 0 can't talk.
-	return <p > I 'm a person and I am {Math.floor(Math.random() * 30) + 8} years old!</p>
+const person = props => {
+  // The + 8 is so 0 is never a number. Someone who is 0 can't talk.
+  return (
+    <p>
+      I'm {props.name} and I am {props.age} years old!
+    </p>
+  );
 };
 
 export default person;
