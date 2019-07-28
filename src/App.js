9c836b90b258d4e2a6a9754200f25d604d8365ae
BLACKBOX\dakil <dakilangmanok@gmail.com>
2019-07-28 14:08:07 -0400

    Realized that the app was a function and not a class. Added state.

--- src/App.js
+++ src/App.js
@@ -1,21 +1,24 @@
-import React from 'react';
+import React, { Component } from 'react';
 import './App.css';
 import Person from './Person/Person';
 
-function App() {
-  return (
-    <div className="App">
-      <h1> Hello world from React! </h1>
-      <p> I'm really working!</p>
-      <button>Switch Name</button>
-      {/* You can pass anything not just text between the opening and closing tag. */}
-      <Person name="Patrick" age="38">
-        My Hobbies: piano
-      </Person>
-      <Person name="John" age="25" />
-      <Person name="Jane" age="29" />
-    </div>
-  );
+class App extends Component {
+  state = {};
+  render() {
+    return (
+      <div className="App">
+        <h1> Hello world from React! </h1>
+        <p> I'm really working!</p>
+        <button>Switch Name</button>
+        {/* You can pass anything not just text between the opening and closing tag. */}
+        <Person name="Patrick" age="38">
+          My Hobbies: piano
+        </Person>
+        <Person name="John" age="25" />
+        <Person name="Jane" age="29" />
+      </div>
+    );
+  }
 }
 
 export default App;
