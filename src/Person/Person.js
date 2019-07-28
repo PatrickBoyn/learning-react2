973d926fc0294c149b0c48be96713ef5e47e704d
BLACKBOX\dakil <dakilangmanok@gmail.com>
2019-07-28 12:39:28 -0400

    Still toying with formatters, I'll try this one for now.

--- src/Person/Person.js
+++ src/Person/Person.js
@@ -2,7 +2,7 @@ import React from 'react';
 
 const person = () => {
 	// The + 8 is so 0 is never a number. Someone who is 0 can't talk.
-	return <p> I 'm a person and I am {Math.floor(Math.random() * 30) + 8} years old!</p>;
+	return <p > I 'm a person and I am {Math.floor(Math.random() * 30) + 8} years old!</p>
 };
 
 export default person;
