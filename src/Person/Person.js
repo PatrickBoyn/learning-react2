29fc5b3e2789e8445f215fa93126757de504ee98
BLACKBOX\dakil <dakilangmanok@gmail.com>
2019-07-28 21:30:51 -0400

    Added a prop for the click attribute.

--- src/Person/Person.js
+++ src/Person/Person.js
@@ -4,7 +4,7 @@ const person = props => {
   // The + 8 is so 0 is never a number. Someone who is 0 can't talk.
   return (
     <div>
-      <p>
+      <p onClick={props.click}>
         I'm {props.name} and I am {props.age} years old!
       </p>
       <p>{props.children}</p>
