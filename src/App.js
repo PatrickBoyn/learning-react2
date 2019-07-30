2a70ee90e1dbb486813c144a51eb91bad7a5440c
BLACKBOX\dakil <dakilangmanok@gmail.com>
2019-07-30 18:08:31 -0400

    Added radium as a dependency.

--- src/App.js
+++ src/App.js
@@ -1,6 +1,7 @@
 import React, { Component } from "react";
 import "./App.css";
 import Person from "./Person/Person";
+import Radium from "radium";
 
 class App extends Component {
   state = {
@@ -102,4 +103,4 @@ class App extends Component {
   }
 }
 
-export default App;
+export default Radium(App);
